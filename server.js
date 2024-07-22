45b507f926195fe5ec2e8e1408e29caa47f05e5e
nathalie <nathalie.metreau@hotmail.fr>
2024-07-22 17:12:51 +0200

    correct eslint task 2

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
+import routes from './routes/index';
+
 const express = require('express');
-import routes from './routes/index.js';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-	app.use('/', routes);
+app.use('/', routes);
 
-	app.listen(port, () => {
-	  console.log(`Server is running on port ${port}`);
-	});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
