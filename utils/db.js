a705d3cbce6181714f5a258300724cbf00381330
Judgy53 <9182@holbertonstudents.com>
2024-07-22 15:37:27 +0200

    Task 1 - MongoDb: EsLint compliance

--- utils/db.js
+++ utils/db.js
@@ -1,38 +1,38 @@
 import { MongoClient } from 'mongodb';
 
 class DBClient {
-    #mongoClient = null;
-    #isConnectionAlive = false;
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || '27017';
+    const database = process.env.DB_DATABASE || 'files_manager';
+    this.mongoClient = new MongoClient(`mongodb://${host}:${port}/${database}`, { useUnifiedTopology: true });
+    this.isConnectionAlive = false;
+    this.mongoClient.connect().then(() => {
+      this.isConnectionAlive = true;
+    });
+  }
 
-    constructor() {
-        const host = process.env.DB_HOST || 'localhost';
-        const port = process.env.DB_PORT || '27017';
-        const database = process.env.DB_DATABASE || 'files_manager';
-        this.#mongoClient = new MongoClient(`mongodb://${host}:${port}/${database}`, { useUnifiedTopology: true });
-        this.#mongoClient.connect().then(() => {
-            this.#isConnectionAlive = true;
-        });
-    }
+  isAlive() {
+    return this.isConnectionAlive;
+  }
 
-    isAlive() {
-        return this.#isConnectionAlive;
+  async nbUsers() {
+    if (!this.isAlive()) {
+      return -1;
     }
+    const collection = this.mongoClient.db().collection('users');
+    const count = await collection.countDocuments();
+    return count;
+  }
 
-    async nbUsers() {
-        if (!this.isAlive())
-            return -1;
-        const collection = this.#mongoClient.db().collection('users');
-        const count = await collection.countDocuments();
-        return count;
+  async nbFiles() {
+    if (!this.isAlive()) {
+      return -1;
     }
-
-    async nbFiles() {
-        if (!this.isAlive())
-            return -1;
-        const collection = this.#mongoClient.db().collection('files');
-        const count = await collection.countDocuments();
-        return count;
-    }
+    const collection = this.mongoClient.db().collection('files');
+    const count = await collection.countDocuments();
+    return count;
+  }
 }
 const dbClient = new DBClient();
 export default dbClient;
